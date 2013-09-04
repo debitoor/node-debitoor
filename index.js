e32c261ce338b8767c8bf26ad7b5477cb0a1bb63
Mathias Buus <mathiasbuus@gmail.com>
2013-09-04 09:50:58 +0200

    disabling cookie jar

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function(token) {
 		if (options.uri[0] === '/') {
 			options.uri = 'https://api.debitoor.com/api/v1.0'+options.uri;
 		}
+		options.jar = false;
 		options.headers = options.headers || {};
 		options.headers['x-token'] = token;
 		if (!('json' in options) && callback) {
